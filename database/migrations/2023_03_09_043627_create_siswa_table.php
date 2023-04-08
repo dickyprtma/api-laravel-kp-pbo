87fe50077de01429c57fa771e218b6a419bc8eb0
Dicky Pratama <dzuerst@gmail.com>
2023-04-09 01:30:54 +0700

    sebelum penambahan level siswa

--- database/migrations/2023_03_09_043627_create_siswa_table.php
+++ database/migrations/2023_03_09_043627_create_siswa_table.php
@@ -23,7 +23,7 @@ class CreateSiswaTable extends Migration
             $table->string('password');
             $table->text('motto')->nullable();
             $table->integer('exp')->default(0);
-            $table->integer('coin')->default(0);
+            $table->integer('level')->default(0);
             $table->string('image')->nullable();
         });
     }
